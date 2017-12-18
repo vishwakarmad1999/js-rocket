a7c2afd45a57e2adb0bde4ae08a83029839f355a
vishwakarmad1999 <vishwakarmad1999@gmail.com>
2017-12-19 00:25:53 +0530

    Made a car object and added JS and CSS

---
+++ js/script.js
@@ -0,0 +1,44 @@
+console.log("Hello there friends");
+var x = 1;
+var incrementX = function () {
+	x++;
+}
+var checkSwitch = function () {
+	if (x % 2 == 0) {
+		car.switchCar(true);
+		incrementX();
+	}
+	else {
+		car.switchCar(false);
+		incrementX();
+	}
+}
+		var car = {
+			make : 'Maruti Suzuki',
+			type : 'Swift',
+			color : 'Azure Gray',
+			isTurnedOn : false,
+			numberOfWheels : 4,
+			seats : ['seat1', 
+					 'seat2', 
+					 'seat3', 
+					 'seat4'],
+			turnOn : function () { 
+				if (this.isTurnedOn == false) 
+					{ 
+						this.isTurnedOn = true;
+					}
+				},
+			fly : function () {
+					alert('fly');
+				},
+			switchCar : function(isOn) {
+				console.log('Car is : ' + isOn); /* true - car is on else false */
+				if (isOn == true) {
+					this.isTurnedOn = true;
+				}
+				else {
+					this.isTurnedOn = false;
+				}
+			}
+		};
